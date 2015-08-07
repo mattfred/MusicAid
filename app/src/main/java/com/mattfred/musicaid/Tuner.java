4553f2b46b3d3027e4da6e3dedfab293d72f4bd5
Matthew Frederick <matthew.frederick@cru.org>
2015-08-07 14:42:42 -0400

    add tuner section

--- app/src/main/java/com/mattfred/musicaid/Tuner.java
+++ app/src/main/java/com/mattfred/musicaid/Tuner.java
@@ -1,9 +1,12 @@
 package com.mattfred.musicaid;
 
 import android.app.AlertDialog;
+import android.content.ComponentName;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
@@ -23,31 +26,66 @@ import android.widget.TextView;
 
 import com.mattfred.musicaid.tuner.PitchView;
 
+import org.puredata.android.io.AudioParameters;
+import org.puredata.android.service.PdService;
+import org.puredata.android.utils.PdUiDispatcher;
+import org.puredata.core.PdBase;
+import org.puredata.core.PdListener;
+import org.puredata.core.utils.IoUtils;
+
+import java.io.File;
+import java.io.IOException;
+
 import static com.mattfred.musicaid.utils.Constants.ABOUT_FRAGMENT;
 import static com.mattfred.musicaid.utils.Constants.METRONOME;
 import static com.mattfred.musicaid.utils.Constants.TRANSPOSE;
 
-public class Tuner extends AppCompatActivity
+public class Tuner extends AppCompatActivity implements View.OnClickListener
 {
     private final String TAG = this.getClass().getSimpleName();
 
-    //private PdUiDispatcher dispatcher;
+    private PdUiDispatcher dispatcher;
 
     private Button eButton;
     private Button aButton;
     private Button dButton;
     private Button gButton;
     private Button bButton;
-    private Button eeButton;
+    private Button e2Button;
     private TextView pitchLabel;
     private PitchView pitchView;
 
-    //private PdService pdService = null;
+    private PdService pdService = null;
 
     private String[] mActivityTitles;
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
     private ActionBarDrawerToggle mDrawerToggle;
+
+    private final ServiceConnection pdConnection = new ServiceConnection()
+    {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service)
+        {
+            pdService = ((PdService.PdBinder)service).getService();
+            try
+            {
+                initPd();
+                loadPatch();
+            }
+            catch (IOException e)
+            {
+                Log.e(TAG, e.toString());
+                finish();
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name)
+        {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -91,6 +129,78 @@ public class Tuner extends AppCompatActivity
         };
 
         mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        initGui();
+        bindService(new Intent(this, PdService.class), pdConnection, BIND_AUTO_CREATE);
+    }
+
+    private void initGui()
+    {
+        setContentView(R.layout.tuner);
+        eButton = (Button) findViewById(R.id.e_button);
+        eButton.setOnClickListener(this);
+        aButton = (Button) findViewById(R.id.a_button);
+        aButton.setOnClickListener(this);
+        dButton = (Button) findViewById(R.id.d_button);
+        dButton.setOnClickListener(this);
+        gButton = (Button) findViewById(R.id.g_button);
+        gButton.setOnClickListener(this);
+        bButton = (Button) findViewById(R.id.b_button);
+        bButton.setOnClickListener(this);
+        e2Button = (Button) findViewById(R.id.e2_button);
+        e2Button.setOnClickListener(this);
+
+        pitchLabel = (TextView) findViewById(R.id.pitch_label);
+
+        pitchView = (PitchView) findViewById(R.id.pitch_view);
+        pitchView.setCenterPitch(45);
+        pitchLabel.setText("A-String");
+    }
+
+    private void triggerNote(int n) {
+        PdBase.sendFloat("midinote", n);
+        PdBase.sendBang("trigger");
+        pitchView.setCenterPitch(n);
+    }
+
+    private void initPd() throws IOException {
+        //Configure the audio glue
+        int sampleRate = AudioParameters.suggestSampleRate();
+        pdService.initAudio(sampleRate,  1 , 2, 10.0f);
+        pdService.startAudio();
+
+        //Create and install the dispatcher
+        dispatcher = new PdUiDispatcher();
+        PdBase.setReceiver(dispatcher);
+        dispatcher.addListener("pitch", new PdListener.Adapter()
+        {
+            @Override
+            public void receiveFloat(String source, float x)
+            {
+                pitchView.setCurrentPitch(x);
+            }
+        });
+    }
+
+    private void loadPatch() throws IOException {
+        File dir = getFilesDir();
+        IoUtils.extractZipResource(getResources().openRawResource(R.raw.bitcrush), dir, true);
+        File patchFile = new File(dir, "bitcrush.pd");
+        PdBase.openPatch(patchFile.getAbsolutePath());
+    }
+
+    private void start() {
+        if (!pdService.isRunning()) {
+            Intent intent = new Intent(Tuner.this, Tuner.class);
+            pdService.startAudio(intent, R.drawable.icon, "GuitarTuner", "Return to Guitar Tuner.");
+        }
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        unbindService(pdConnection);
     }
 
     @Override
@@ -173,6 +283,37 @@ public class Tuner extends AppCompatActivity
         mDrawerLayout.closeDrawer(mDrawerList);
 
         goToNewActivity(position);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        switch (v.getId()) {
+            case R.id.e_button:
+                triggerNote(40);
+                pitchLabel.setText("E-String");
+                break;
+            case R.id.a_button:
+                triggerNote(45);
+                pitchLabel.setText("A-String");
+                break;
+            case R.id.d_button:
+                triggerNote(50);
+                pitchLabel.setText("D-String");
+                break;
+            case R.id.g_button:
+                triggerNote(55);
+                pitchLabel.setText("G-String");
+                break;
+            case R.id.b_button:
+                triggerNote(59);
+                pitchLabel.setText("B-String");
+                break;
+            case R.id.e2_button:
+                triggerNote(60);
+                pitchLabel.setText("E-String");
+                break;
+        }
     }
 
     private class DrawerItemClickListener implements ListView.OnItemClickListener {
